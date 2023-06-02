c1473b0d8a5e6e6029919e21645a0dcdf562dc62
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-06-02 14:26:25 -0500

    correccion identado

--- ejercicio3/test_esEscalar.py
+++ ejercicio3/test_esEscalar.py
@@ -1,10 +1,10 @@
 import esEscalar as fu
 
-    def prueba(M):
-        if (fu.esEscalar(M)):
-            print("Si es escalar")
-        else:
-            print("No es escalar")
+def prueba(M):
+    if (fu.esEscalar(M)):
+        print("Si es escalar")
+    else:
+        print("No es escalar")
 
 #Z = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 #Z = [[1, 2, 3], [4, 1, 6], [7, 8, 1]] 
